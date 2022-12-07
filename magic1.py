9e210e9b8913c2789e2303fbba1e46b9cd755957
Lutmanova_Ekaterina <elutmanova817@gmail.com>
2022-12-07 18:46:07 +0300

    encapsulation

---
+++ magic1.py
@@ -0,0 +1,17 @@
+class Bottle:
+    color = 'Красная'
+    value = 0.0
+
+bottle_1 = Bottle()
+bottle_2 = Bottle()
+bottle_3 = Bottle()
+
+setattr(bottle_1, 'value', 0.7)
+bottle_2.color = 'Белая'
+bottle_2.value = 0.3
+bottle_3.color = 'Чёрная'
+bottle_3.value = 1.0
+
+print(bottle_1.color, bottle_1.value)
+print(bottle_2.color, bottle_2.value)
+print(bottle_3.color, bottle_3.value)
